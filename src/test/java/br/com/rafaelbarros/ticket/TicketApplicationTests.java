73f64d51436ccada24fe506c4164ca06a102bfda
Rafael Barros <br.rafaelbarros@gmail.com>
2024-02-14 11:48:07 -0300

    conf: improve base structure to test

--- src/test/java/br/com/rafaelbarros/ticket/TicketApplicationTests.java
+++ src/test/java/br/com/rafaelbarros/ticket/TicketApplicationTests.java
@@ -1,13 +1,21 @@
 package br.com.rafaelbarros.ticket;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class TicketApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertNotNull(applicationContext, "The application context should have loaded!");
 	}
 
 }
